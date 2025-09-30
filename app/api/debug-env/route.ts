b0f8a73db7e90932dcace9a27806259a7d6c2e8a
Bishal-Muherjee <bishalmukherjee448gmail.com>
2025-09-30 20:35:18 +0530

    feat: updated debug controller response

--- app/api/debug-env/route.ts
+++ app/api/debug-env/route.ts
@@ -4,6 +4,6 @@ export const GET = async (request: NextRequest) => {
   return NextResponse.json({
     DB_HOST: process.env.DB_HOST,
     NODE_ENV: process.env.NODE_ENV,
-    allEnvKeys: Object.keys(process.env).filter((key) => key.startsWith("DB_")),
+    allEnvKeys: Object.keys(process.env),
   });
 };
